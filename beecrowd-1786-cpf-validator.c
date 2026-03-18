98796506ea65fba079e9aac26b1b4c50999e8a4a
Pedro Weller <pedroweller939@gmail.com>
2026-03-17 22:16:04 -0300

    Update beecrowd-1786-cpf-validator.c
    feat: add EOF loop for multiple test cases.

--- beecrowd-1786-cpf-validator.c
+++ beecrowd-1786-cpf-validator.c
@@ -1,12 +1,14 @@
 
 #include <stdio.h>
+#include <stdio.h>
  
 int main() {
     
     int n1,n2,n3,n4,n5,n6,n7,n8,n9,b1,b2 = 0;
     
     
-    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9 );
+    while(scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9) != EOF){
+	
     
     b1 = (n1*1+n2*2+n3*3+n4*4+n5*5+n6*6+n7*7+n8*8+n9*9) % 11;
     b2 = (n1*9+n2*8+n3*7+n4*6+n5*5+n6*4+n7*3+n8*2+n9*1) % 11;
@@ -23,7 +25,7 @@ int main() {
     }
     
     
-    printf("%d%d%d.%d%d%d.%d%d%d-%d%d", n1,n2,n3,n4,n5,n6,n7,n8,n9,b1,b2);
-
+    printf("%d%d%d.%d%d%d.%d%d%d-%d%d\n", n1,n2,n3,n4,n5,n6,n7,n8,n9,b1,b2);
+}
     return 0;
 }
